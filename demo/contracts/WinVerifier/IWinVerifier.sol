be27602e0cf943b8af9d317aaa73d94e7d34df4d
heejuyeo <yeohuiju03@gmail.com>
2025-11-13 05:31:30 +0900

    [FEAT] WinVerifier 시그니처 변경

--- demo/contracts/WinVerifier/IWinVerifier.sol
+++ demo/contracts/WinVerifier/IWinVerifier.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.19;
 
 interface IWinVerifier {
-    function verifyProof(
+    function verifyWinProof(
         uint256[24] calldata proof,
         uint256[3] calldata pubSignals
     ) external view returns (bool);
